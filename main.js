de4cd5273e4c11a21217fa8b541fb678befdd2f0
TA2k <tombox2020@gmail.com>
2021-07-21 22:01:13 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,305 @@
+"use strict";
+
+/*
+ * Created with @iobroker/create-adapter v1.34.1
+ * Based on https://github.com/nVuln/homebridge-lg-thinq
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require("@iobroker/adapter-core");
+const axios = require("axios");
+const crypto = require("crypto");
+const qs = require("qs");
+const { DateTime } = require("luxon");
+const { extractKeys } = require("./lib/extractKeys");
+const constants = require("./lib/constants");
+class LgThinq extends utils.Adapter {
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: "lg-thinq",
+        });
+        this.on("ready", this.onReady.bind(this));
+        // this.on("stateChange", this.onStateChange.bind(this));
+        this.on("unload", this.onUnload.bind(this));
+    }
+
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    async onReady() {
+        this.setState("info.connection", false, true);
+        if (this.config.interval < 0.5) {
+            this.log.info("Set interval to minimum 0.5");
+            this.config.interval = 0.5;
+        }
+        this.requestClient = axios.create();
+        this.defaultHeaders = {
+            "x-api-key": constants.API_KEY,
+            "x-client-id": constants.API_CLIENT_ID,
+            "x-thinq-app-ver": "3.5.1700",
+            "x-thinq-app-type": "NUTS",
+            "x-thinq-app-level": "PRD",
+            "x-thinq-app-os": "ANDROID",
+            "x-thinq-app-logintype": "LGE",
+            "x-service-code": "SVC202",
+            "x-country-code": this.config.country,
+            "x-language-code": this.config.language,
+            "x-service-phase": "OP",
+            "x-origin": "app-native",
+            "x-model-name": "samsung / SM-N950N",
+            "x-os-version": "7.1.2",
+            "x-app-version": "3.5.1721",
+            "x-message-id": this.random_string(22),
+        };
+
+        // if (this.session.accessToken) {
+        //   headers['x-emp-token'] = this.session.accessToken;
+        // }
+
+        // if (this.userNumber) {
+        //   headers['x-user-no'] = this.userNumber;
+        // }
+
+        this.gateway = await this.requestClient.get(constants.GATEWAY_URL, { headers: this.defaultHeaders }).then((res) => res.data.result);
+        this.session = await this.login(this.config.user, this.config.password);
+        this.userNumber = await this.getUserNumber(this.session.accessToken);
+    }
+
+    async login(username, password) {
+        // get signature and timestamp in login form
+        const loginForm = await this.requestClient.get(await this.getLoginUrl()).then((res) => res.data);
+        const headers = {
+            Accept: "application/json",
+            "X-Application-Key": constants.APPLICATION_KEY,
+            "X-Client-App-Key": constants.CLIENT_ID,
+            "X-Lge-Svccode": "SVC709",
+            "X-Device-Type": "M01",
+            "X-Device-Platform": "ADR",
+            "X-Device-Language-Type": "IETF",
+            "X-Device-Publish-Flag": "Y",
+            "X-Device-Country": this.gateway.country_code,
+            "X-Device-Language": this.gateway.language_code,
+            "X-Signature": loginForm.match(/signature\s+:\s+"([^"]+)"/)[1],
+            "X-Timestamp": loginForm.match(/tStamp\s+:\s+"([^"]+)"/)[1],
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        };
+
+        const hash = crypto.createHash("sha512");
+        const data = {
+            user_auth2: hash.update(password).digest("hex"),
+            itg_terms_use_flag: "Y",
+            svc_list: "SVC202,SVC710", // SVC202=LG SmartHome, SVC710=EMP OAuth
+        };
+
+        // try login with username and hashed password
+        const loginUrl = this.gateway.emp_base_url + "emp/v2.0/account/session/" + encodeURIComponent(username);
+        const res = await this.requestClient
+            .post(loginUrl, qs.stringify(data), { headers })
+            .then((res) => res.data)
+            .catch((err) => {
+                if (!err.response) {
+                    throw err;
+                }
+
+                const { code, message } = err.response.data.error;
+                if (code === "MS.001.03") {
+                    this.log.error("Double-check your country in configuration");
+                }
+
+                this.log.error(message);
+            });
+
+        // dynamic get secret key for emp signature
+        const empSearchKeyUrl = this.gateway.login_base_url + "searchKey?key_name=OAUTH_SECRETKEY&sever_type=OP";
+        const secretKey = await this.requestClient
+            .get(empSearchKeyUrl)
+            .then((res) => res.data)
+            .then((data) => data.returnData);
+
+        const timestamp = DateTime.utc().toRFC2822();
+        const empData = {
+            account_type: res.account.userIDType,
+            client_id: constants.CLIENT_ID,
+            country_code: res.account.country,
+            username: res.account.userID,
+        };
+        const empUrl = "/emp/oauth2/token/empsession" + qs.stringify(empData, { addQueryPrefix: true });
+        const signature = this.signature(`${empUrl}\n${timestamp}`, secretKey);
+        const empHeaders = {
+            "lgemp-x-app-key": constants.OAUTH_CLIENT_KEY,
+            "lgemp-x-date": timestamp,
+            "lgemp-x-session-key": res.account.loginSessionID,
+            "lgemp-x-signature": signature,
+            Accept: "application/json",
+            "X-Device-Type": "M01",
+            "X-Device-Platform": "ADR",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Access-Control-Allow-Origin": "*",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9",
+        };
+        // create emp session and get access token
+        const token = await this.requestClient
+            .post("https://emp-oauth.lgecloud.com/emp/oauth2/token/empsession", qs.stringify(empData), {
+                headers: empHeaders,
+            })
+            .then((res) => res.data)
+            .catch((err) => {
+                this.log.error(err.response.data.error.message);
+            });
+        if (token.status !== 1) {
+            this.log.error(token.message);
+        }
+
+        this.lgeapi_url = token.oauth2_backend_url || this.lgeapi_url;
+
+        // login to old gateway also - thinq v1
+        const memberLoginUrl = this.gateway.thinq1_url + "member/login";
+        const memberLoginHeaders = {
+            "x-thinq-application-key": "wideq",
+            "x-thinq-security-key": "nuts_securitykey",
+            Accept: "application/json",
+            "x-thinq-token": token.access_token,
+        };
+        const memberLoginData = {
+            countryCode: this.gateway.country_code,
+            langCode: this.gateway.language_code,
+            loginType: "EMP",
+            token: token.access_token,
+        };
+        this.jsessionId = await this.requestClient
+            .post(
+                memberLoginUrl,
+                { lgedmRoot: memberLoginData },
+                {
+                    headers: memberLoginHeaders,
+                }
+            )
+            .then((res) => res.data)
+            .then((data) => data.lgedmRoot.jsessionId);
+
+        return { access_token: token.access_token, refresh_token: token.refresh_token, expires_in: token.expires_in };
+    }
+
+    async refreshNewToken(session) {
+        const tokenUrl = this.lgeapi_url + "oauth2/token";
+        const data = {
+            grant_type: "refresh_token",
+            refresh_token: session.refreshToken,
+        };
+
+        const timestamp = DateTime.utc().toRFC2822();
+
+        const requestUrl = "/oauth2/token" + qs.stringify(data, { addQueryPrefix: true });
+        const signature = this.signature(`${requestUrl}\n${timestamp}`, constants.OAUTH_SECRET_KEY);
+
+        const headers = {
+            "lgemp-x-app-key": constants.CLIENT_ID,
+            "lgemp-x-signature": signature,
+            "lgemp-x-date": timestamp,
+            Accept: "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+        };
+        const resp = await this.requestClient.post(tokenUrl, qs.stringify(data), { headers }).then((resp) => resp.data);
+
+        this.session.access_token = resp.access_token;
+    }
+
+    async getUserNumber(accessToken) {
+        const profileUrl = this.lgeapi_url + "users/profile";
+        const timestamp = DateTime.utc().toRFC2822();
+        const signature = this.signature(`/users/profile\n${timestamp}`, constants.OAUTH_SECRET_KEY);
+
+        const headers = {
+            Accept: "application/json",
+            Authorization: "Bearer " + accessToken,
+            "X-Lge-Svccode": "SVC202",
+            "X-Application-Key": constants.APPLICATION_KEY,
+            "lgemp-x-app-key": constants.CLIENT_ID,
+            "X-Device-Type": "M01",
+            "X-Device-Platform": "ADR",
+            "x-lge-oauth-date": timestamp,
+            "x-lge-oauth-signature": signature,
+        };
+
+        const resp = await this.requestClient.get(profileUrl, { headers }).then((resp) => resp.data);
+        return resp.account.userNo;
+    }
+
+    async getLoginUrl() {
+        const params = {
+            country: this.gateway.country_code,
+            language: this.gateway.language_code,
+            client_id: constants.CLIENT_ID,
+            svc_list: constants.SVC_CODE,
+            svc_integrated: "Y",
+            redirect_uri: this.gateway.login_base_url + "login/iabClose",
+            show_thirdparty_login: "LGE,MYLG",
+            division: "ha:T20",
+            callback_url: this.gateway.login_base_url + "login/iabClose",
+        };
+
+        return this.gateway.login_base_url + "login/signIn" + qs.stringify(params, { addQueryPrefix: true });
+    }
+
+    signature(message, secret) {
+        return crypto.createHmac("sha1", Buffer.from(secret)).update(message).digest("base64");
+    }
+    random_string(length) {
+        const result = [];
+        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        const charactersLength = characters.length;
+        for (let i = 0; i < length; i++) {
+            result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
+        }
+        return result.join("");
+    }
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            // Here you must clear all timeouts or intervals that may still be active
+            // clearTimeout(timeout1);
+            // clearTimeout(timeout2);
+            // ...
+            // clearInterval(interval1);
+
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+    /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     * @param {ioBroker.State | null | undefined} state
+     */
+    onStateChange(id, state) {
+        if (state) {
+            // The state was changed
+            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+        } else {
+            // The state was deleted
+            this.log.info(`state ${id} deleted`);
+        }
+    }
+}
+
+if (require.main !== module) {
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new LgThinq(options);
+} else {
+    // otherwise start the instance directly
+    new LgThinq();
+}
